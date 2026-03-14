ce6ed7a6cac9632ad68cffe77a6713ee59c7f8e9
ihibti <ismaelhibti2000@gmail.com>
2026-03-14 14:14:18 +0100

    Remove ./lc script and setup command

--- xtask/src/setup.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-
-pub fn run() -> Result<(), io::Error> {
-    println!("Note: 'cargo setup' has been replaced by './lc setup'.");
-    println!("Run ./lc setup from the repo root instead.");
-    println!("(./lc works even before Rust/Cargo are installed.)");
-    Ok(())
-}
